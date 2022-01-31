7943097dbaa33f23c26b73bbcdcd9a7b3ba27ff8
Константин Черника <berrykot@gmail.com>
2022-01-31 17:57:56 +0300

    Init

---
+++ src/main/java/com/example/prikol/PrikolApplication.java
@@ -0,0 +1,13 @@
+package com.example.prikol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrikolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrikolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prikol/PrikolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prikol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrikolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
