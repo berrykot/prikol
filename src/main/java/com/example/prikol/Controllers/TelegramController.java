48f725e9d6220cca68cd838f69d865f0250f22f0
Константин Черника <berrykot@gmail.com>
2022-01-31 21:15:04 +0300

    Smth

--- src/main/java/com/example/prikol/Controllers/TelegramController.java
+++ src/main/java/com/example/prikol/Controllers/TelegramController.java
@@ -1,8 +1,12 @@
 package com.example.prikol.Controllers;
 
+import com.pengrad.telegrambot.model.*;
+import com.pengrad.telegrambot.model.request.*;
+
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +18,15 @@ public class TelegramController {
     final String telegramUrl = "https://api.telegram.org/bot5180751190:AAFCfs7DqJCVvVfvqz9bId-bLc8JFUpeYuk";
 
     @RequestMapping("/")
-    //@ResponseBody
-    public String index(@RequestBody String update) {
-        if(update == null)
-            update= "Empty";
-
-        System.out.println(update);
+    @ResponseBody
+    public Update index(@RequestBody Update update) {
 
         var response = Unirest.post(telegramUrl + "/sendMessage")
                 .header("accept", "application/json")
-                .field("chat_id", "104830286")
-                .field("text", update)
+                .field("chat_id", update.message().from().id())
+                .field("text", "Привет, " + update.message().from().firstName())
                 .asJson();
 
-        return response.getBody().toString();
+        return update;
     }
 }
