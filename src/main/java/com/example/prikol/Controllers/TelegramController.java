ebfa2387f637dded28e601c0e453010a44ddc6c2
Константин Черника <berrykot@gmail.com>
2022-02-01 12:16:59 +0300

    smth

--- src/main/java/com/example/prikol/Controllers/TelegramController.java
+++ src/main/java/com/example/prikol/Controllers/TelegramController.java
@@ -2,8 +2,12 @@ package com.example.prikol.Controllers;
 
 //import com.example.prikol.Models.*;
 import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.*;
 
+import com.pengrad.telegrambot.model.request.ForceReply;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
 import kong.unirest.Unirest;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,18 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TelegramController {
 
     final String telegramUrl = "https://api.telegram.org/bot5180751190:AAFCfs7DqJCVvVfvqz9bId-bLc8JFUpeYuk";
+    private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
 
     @RequestMapping("/")
     @ResponseBody
-    public String index(@RequestBody String request) {
+    public void index(@RequestBody String request) {
         var update = BotUtils.parseUpdate(request);
+        var chatId = update.message().from().id();
+        var username = update.message().from().username();
+        var text = update.message().text();
 
-        var response = Unirest.post(telegramUrl + "/sendMessage")
-                .header("accept", "application/json")
-                .field("chat_id", update.message().from().id())
-                .field("text", "Привет, " + update.message().from().username())
-                .asJson();
-
-        return BotUtils.toJson(update);
+        var requestSendMessage = new SendMessage(chatId, username + ", Вы сказали, " + text);
+        bot.execute(requestSendMessage);
     }
 }
